d790bdff40dfa4b8c065b36db20c51edc87553d5
dergash <dergachev@hotmail.com>
2021-01-26 17:04:43 +0300

    ViewMeta json schema

--- src/interfaces/ViewMetaJson.d.ts
+++ src/interfaces/ViewMetaJson.d.ts
@@ -1,16 +1,49 @@
-import type { WidgetMeta } from '@tesler-ui/core/interfaces/widget'
-
 export interface ViewMetaJson {
-    id: number
+    /**
+     * Unique identifier for the view
+     */
     name: string
-    title?: string
-    template?: string
-    customizable?: boolean
-    editable?: boolean
+    /**
+     * Displayed name
+     */
+    title: string
+    /**
+     * Url for the view (usually in form of `${screen.name}/${view.name}`)
+     */
     url: string
-    widgets: WidgetMeta[]
-    columns: number | null
-    rowHeight: number | null
-    readOnly: boolean
-    ignoreHistory?: boolean
+    /**
+     * Widgets present on the view
+     */
+    widgets: ViewMetaWidget
+}
+
+type ViewMetaWidget = ViewMetaWidgetNumber | ViewMetaWidgetString
+
+interface ViewMetaWidgetBase {
+    position: number
+    gridWidth?: number
+}
+
+/**
+ * Widget can be uniquely identified by numeric id
+ *
+ * @deprecated TODO: Will be removed in Tesler 2.0.0
+ */
+interface ViewMetaWidgetNumber extends ViewMetaWidgetBase {
+    /**
+     * Reference to `id` property from *.widget.json file
+     *
+     * @deprecated TODO: Will be removed in Tesler 2.0.0
+     */
+    widgetId: number
+}
+
+/**
+ * Widget can be uniquely identified by string id
+ */
+interface ViewMetaWidgetString extends ViewMetaWidgetBase {
+    /**
+     * Reference to `name` property from *.widget.json file
+     */
+    widgetName: string
 }
