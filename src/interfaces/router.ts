328fbcab2a2d42cec841fb0f7afd1598b5a53b4a
Fedor Gubarev <gubarevfyodor@gmail.com>
2021-07-03 21:44:47 +0300

    Duplications of const enums are added for compatibility  with isolatedModules flag

--- src/interfaces/router.ts
+++ src/interfaces/router.ts
@@ -18,7 +18,34 @@
 /**
  * Types of drilldowns in the application, specified by Tesler API
  */
-export enum DrillDownType {
+export const enum DrillDownType {
+    /**
+     * Drilldown to inner entity of the application (screen, view), i.e. url will be places after route hash sy: `#/${inner}`
+     */
+    inner = 'inner',
+    /**
+     * Drilldown to an url relative to the current url: `/${relative}`
+     */
+    relative = 'relative',
+    /**
+     * Drilldown to an url relative to the current url: `/${relative}` that opens in a new browser tab
+     */
+    relativeNew = 'relativeNew',
+    /**
+     * An external redirect, i.e. `http://${external}`
+     */
+    external = 'external',
+    /**
+     * An external redirect, i.e. `http://${external}` that opens in a new browser tab
+     */
+    externalNew = 'externalNew'
+}
+
+/**
+ * Types of drilldowns in the application, specified by Tesler API
+ *
+ */
+export enum DrillDownTypeEnum {
     /**
      * Drilldown to inner entity of the application (screen, view), i.e. url will be places after route hash sy: `#/${inner}`
      */
